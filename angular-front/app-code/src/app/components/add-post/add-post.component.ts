2f8ab9397128c0016613d09606c244f6203b6b3e
saladinjake <juwavictor@gmail.com>
2020-10-27 19:44:16 +0100

    Consume api with frontend by implementing create post and post list

--- angular-front/app-code/src/app/components/add-post/add-post.component.ts
+++ angular-front/app-code/src/app/components/add-post/add-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post-service.service';
 
 @Component({
   selector: 'app-add-post',
@@ -7,7 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPostComponent implements OnInit {
 
-  constructor() { }
+  post = {
+    title: '',
+    description: '',
+    published: false
+  };
+  submitted = false;
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit(): void {
+  }
+
+  savePost(): void {
+    const data = {
+      title: this.post.title,
+      description: this.post.description
+    };
+
+    this.postService.createPost(data)
+      .subscribe(
+        response => {
+
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  newPostReset(): void {
+    this.submitted = false;
+    this.post = {
+      title: '',
+      description: '',
+      published: false
+    };
+  }
 
   ngOnInit(): void {
   }
--- angular-front/app-code/src/app/components/post-list/post-list.component.ts
+++ angular-front/app-code/src/app/components/post-list/post-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post-service.service';
+
 
 @Component({
   selector: 'app-post-list',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+  posts: any;
+  currentPost = null;
+  currentIndex = -1;
+  title = '';
+
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.getPosts();
   }
+
+  getPosts(): void {
+    this.postService.getAllPosts()
+      .subscribe(
+        data => {
+          this.posts = data;
+
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  refreshList(): void {
+    this.getPosts();
+    this.currentPost = null;
+    this.currentIndex = -1;
+  }
+
+  setPostInView(tutorial, index): void {
+    this.currentPost = tutorial;
+    this.currentIndex = index;
+  }
+
+  removeAllPosts(): void {
+    this.postService.deleteAll()
+      .subscribe(
+        response => {
+          console.log(response);
+          this.getPosts();
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  searchTitle(): void {
+    this.postService.findByTitle(this.title)
+      .subscribe(
+        data => {
+          this.posts = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
 
 }
