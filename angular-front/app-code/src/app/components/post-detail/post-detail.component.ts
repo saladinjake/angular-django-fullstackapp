06feeed187214ebc3ecfe0b5bfb39acdb2f2eb04
saladinjake <juwavictor@gmail.com>
2020-10-27 19:54:49 +0100

    Consume api with frontend by implementing post edit

--- angular-front/app-code/src/app/components/post-detail/post-detail.component.ts
+++ angular-front/app-code/src/app/components/post-detail/post-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
@@ -7,9 +9,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDetailComponent implements OnInit {
 
-  constructor() { }
+  currentPost = null;
+  message = '';
+
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getPostById(this.route.snapshot.paramMap.get('id'));
+  }
+
+  getPostById(id): void {
+    this.postService.getById(id)
+      .subscribe(
+        data => {
+          this.currentPost = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updatePublished(status): void {
+    const data = {
+      title: this.currentPost.title,
+      description: this.currentPost.description,
+      published: status
+    };
+
+    this.postService.updatePost(this.currentPost.id, data)
+      .subscribe(
+        response => {
+          this.currentTutorial.published = status;
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updatePost(): void {
+    this.postService.updatePost(this.currentPost.id, this.currentPost)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = 'This post was update was successful!';
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  deletePost(): void {
+    this.postService.deletePost(this.currentPost.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/posts']);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
