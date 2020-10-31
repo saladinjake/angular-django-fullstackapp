88169e151be3a3f0b834ad454e4014f43d05fe99
saladinjake <juwavictor@gmail.com>
2020-10-31 21:14:31 +0100

    bg:Fix broken links

--- Alldjangoprojects/simpleprojectmanagement/urls.py
+++ Alldjangoprojects/simpleprojectmanagement/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from simpleprojectmanagement import views
 
 urlpatterns = [
-    url(r'^home', views.index,  name="public_index"),
+    url(r'^$', views.index,  name="public_index"),
     url(r'^auth/login', views.login,  name="public_login"),
     url(r'^auth/register', views.register,  name="public_register"),
 
