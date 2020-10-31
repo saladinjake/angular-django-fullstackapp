6a44ddc156d30c05d60560bc1cabe2822f681916
saladinjake <juwavictor@gmail.com>
2020-10-31 14:39:31 +0100

    Add crud operation for project model

--- Alldjangoprojects/simpleprojectmanagement/urls.py
+++ Alldjangoprojects/simpleprojectmanagement/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     url(r'^auth/login', views.login,  name="public_login"),
     url(r'^auth/register', views.register,  name="public_register"),
 
+    #project urls
+    url(r'^all_projects', views.get_all_projects, name="all_projects"),
+    url(r'^project_detail-(?P<pk>\d+)', views.project_detail, name="project_detail"),
+    url(r'^project_update-(?P<pk>\d+)', views.project_update name="project_update"),
+    url(r'^project_delete-(?P<pk>\d+)', views.project_delete name="project_delete"),
 ]
--- Alldjangoprojects/simpleprojectmanagement/views.py
+++ Alldjangoprojects/simpleprojectmanagement/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
+from simpleprojectmanagement.models import Project,Developer,Supervisor, UserProfile, Task
+
+"""
+ guest and auth pages
+"""
 
 def index(request):
     return render(request, 'public/index.html')
@@ -10,3 +16,83 @@ def login(request):
 
 def register(request):
     return render(request, 'public/register.html')
+
+'''
+ project crud operations
+'''
+
+def create_project(request):
+     if request.POST:
+         if 'title' in request.POST:
+             name = request.POST.get('title', '')
+         else:
+             error=True
+         if 'description' in request.POST:
+             description = request.POST.get('description', '')
+         else:
+             error=True
+         if 'client_name' in request.POST:
+             password = request.POST.get('client_name', '')
+         else:
+             error=True
+         if not error:
+             new_project = Project(title="Build toptal client", description="Toptal project crud app",client_name="Toptal") # line 2
+             new_project.save() # line 3
+             return render(request, 'public/create_project.html', {'action':'Save successful'})
+         else:
+            return render(request, 'public/create_project.html', {'action':'Error in form'})
+     else:
+          return render(request, 'public/create_project.html', {'action':'Add a project'})
+
+def get_all_projects(request):
+    all_projects = Project.objects.all()
+    return render(request, 'public/all_projects.html', {'message': "All projects", 'all_projects': all_projects})
+
+def project_detail(request, pk):
+    project = Project.objects.get(id=pk)
+    return render(request, 'public/project_detail.html', {'project' : project})
+
+def project_update(request,pk):
+    project = Project.objects.get(id=pk)
+    project.title = request.POST.get("title", project.title)
+    project.description = request.POST.get("title", project.title)
+    project.client_name = request.POST.get("title", project.title)
+    project.id = request.get("title", pk)
+    return render(request, 'public/project_update.html', {'project' : project})
+
+def project_delete(request,pk):
+    project = Project.objects.get(id = 1)
+    project.delete() # line 1
+
+    return render(request, 'public/project_detail.html', {'action' : 'Delete was successful'})
+
+def project_flush(request):
+    all_tasks = Task.objects.all()
+    all_tasks.delete() # line 2
+    return render(request, 'public/project_detail.html', {'action' : 'Delete tasks'})
+
+
+'''
+ developer crud operations
+'''
+
+
+
+
+
+'''
+ supervisor crud operations
+'''
+
+
+
+
+'''
+ user crud operations
+'''
+
+
+
+'''
+ task crud operations
+'''
